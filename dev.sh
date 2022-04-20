e5ff6094cffec142eb8bf5e6ca654ef729a755f7
Alan Crosswell <alan@columbia.edu>
2022-04-20 13:48:52 -0400

    build and install latest dev snapshot of pat.

--- dev.sh
+++ dev.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -x
 # the one true editor:
+apt-get update
 apt-get install -y emacs
-apt-get install -y golang-mode
+#apt-get install -y golang-mode
 echo "(add-hook 'before-save-hook #'gofmt-before-save)" >~vagrant/.emacs
 # following to build from source:
 wget -q https://golang.org/dl/go1.17.linux-amd64.tar.gz
@@ -9,12 +11,11 @@ sudo tar -C /usr/local -xzf go1.17.linux-amd64.tar.gz
 echo "export PATH=/usr/local/go/bin:$PATH:~/.go/bin" >> ~vagrant/.profile
 echo "export GOPATH=~/.go" >> ~vagrant/.profile
 /usr/local/go/bin/go install github.com/go-delve/delve/cmd/dlv@latest
-# ~/src is now cross-mounted, so no need to do this cloning etc.
-# mkdir ~vagrant/src
-# cd ~vagrant/src
-# clone my development fork
-# git clone git@github.com:n2ygk/pat.git
-# cd pat
-# git remote add upstream git@github.com:la5nta/pat.git
-# cd ~/vagrant/src
-# chown -R vagrant .
+cd ~/vagrant/src/pat
+git checkout develop
+git pull
+sudo systemctl stop pat@vagrant
+./make.bash libax25
+./make.bash
+sudo install -c pat /usr/bin/pat
+sudo systemctl start pat@vagrant
