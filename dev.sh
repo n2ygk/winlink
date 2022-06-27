95b808ef15a1489c5c16bec6d40767005c151071
Alan Crosswell <alan@columbia.edu>
2022-06-27 15:00:12 -0400

    Include source code and development tools for soundmodem.deb.

--- dev.sh
+++ dev.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -x
 # the one true editor:
-apt-get update
-apt-get install -y emacs
+sudo apt-get update
+sudo apt-get install -y emacs
+sudo apt-get install -y build-essential fakeroot devscripts
+sudo apt-get -y build-dep soundmodem
+# enable rebuilding soundmodem.deb package:
+https://salsa.debian.org/debian-hamradio-team/soundmodem.git
+# this gets an older version that what is distrubted!
+# apt-get source soundmodem
+(cd ~vagrant/src/debian; git clone https://salsa.debian.org/debian-hamradio-team/soundmodem.git)
+echo 'Use "debuild -b -uc -us" to rebuild soundmodem'
+
 #apt-get install -y golang-mode
 echo "(add-hook 'before-save-hook #'gofmt-before-save)" >~vagrant/.emacs
 # following to build from source:
@@ -10,6 +19,8 @@ wget -q https://golang.org/dl/go1.17.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.17.linux-amd64.tar.gz
 echo "export PATH=/usr/local/go/bin:$PATH:~/.go/bin" >> ~vagrant/.profile
 echo "export GOPATH=~/.go" >> ~vagrant/.profile
+export PATH=/usr/local/go/bin:$PATH:~/.go/bin
+export GOPATH=~/.go
 /usr/local/go/bin/go install github.com/go-delve/delve/cmd/dlv@latest
 cd ~/vagrant/src/pat
 git checkout develop
---
+++ dh_make.bashrc
@@ -0,0 +1,2 @@
+export DEBEMAIL="n2ygk@weca.org"
+export DEBFULLNAME="Alan Crosswell"
